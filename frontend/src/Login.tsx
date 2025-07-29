983b4f4552fa854b1917fdc5fd1704094d517fe8
Joao Pedro <joao.pio@hotmail.com>
2025-07-29 19:58:47 -0300

    Fix login page

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import logo from '../src/assets/favicon/android-chrome-512x512.png'
+import './Login.css'
 
 const Login: React.FC = () => {
 	const [username, setUsername] = useState('')
@@ -32,29 +34,33 @@ const Login: React.FC = () => {
 	}
 
 	return (
-		<div style={{ maxWidth: 400, margin: 'auto', paddingTop: '2rem' }}>
-			<h2>Login</h2>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					placeholder="Username"
-					value={username}
-					onChange={e => setUsername(e.target.value)}
-					required
-					style={{ width: '100%', padding: '0.5rem', marginBottom: '1rem' }}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-					required
-					style={{ width: '100%', padding: '0.5rem', marginBottom: '1rem' }}
-				/>
-				<button type="submit" style={{ width: '100%', padding: '0.5rem' }}>
-					Log In
-				</button>
-			</form>
+		<div style={{ maxWidth: 600, margin: 'auto', paddingTop: '2rem', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+			<div className='loginLogoImg logoRadShadow'>
+				<img src={logo} style={{ maxWidth: '200px' }} />
+			</div>
+			<div>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						placeholder="Username"
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+						required
+						style={{ width: '100%', padding: '0.5rem', marginBottom: '1rem' }}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						required
+						style={{ width: '100%', padding: '0.5rem', marginBottom: '1rem' }}
+					/>
+					<button type="submit" style={{ width: '100%', padding: '0.5rem' }}>
+						Log In
+					</button>
+				</form>
+			</div>
 			{error && <p style={{ color: 'red' }}>{error}</p>}
 		</div>
 	)
