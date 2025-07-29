35f04e02f5f990f19522582531433209d945b87e
Joao Pedro <joao.pio@hotmail.com>
2025-07-29 14:09:11 -0300

    Add email service

---
+++ backend/app/infra/email_service.go
@@ -0,0 +1,92 @@
+package infra
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/mail"
+)
+
+const emailServiceAPIUrl = "https://api.postmarkapp.com/email/withTemplate"
+
+type EmailService interface {
+	SendCreateAccountMFACode(mfaCode, toAddress string) error
+	IsValidEmailAddress(email string) bool
+}
+
+func NewEmailService(client *http.Client, emailAPIToken string, fromAddress string) EmailService {
+	return &emailService{
+		client: client,
+		emailAPIToken: emailAPIToken,
+		fromAddress:   fromAddress,
+	}
+}
+
+type emailService struct {
+	client *http.Client
+	emailAPIToken string
+	fromAddress   string
+}
+
+type templateModel struct {
+	MfaCode string `json:"mfa_code"`
+}
+
+type mfaCodeBodyRequest struct {
+	From          string `json:"From"`
+	To            string `json:"To"`
+	TemplateAlias string `json:"TemplateAlias"`
+	TemplateModel *templateModel
+}
+
+func (s *emailService) IsValidEmailAddress(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func (s *emailService) SendCreateAccountMFACode(mfaCode, toAddress string) error {
+	data, err := s.createMfaCodeBodyRequest(mfaCode, s.fromAddress, toAddress)
+	if err != nil {
+		return fmt.Errorf("unable to create mfa code body request: %w", err)
+	}
+	req, err := http.NewRequest("POST", emailServiceAPIUrl, data)
+	if err != nil {
+		return fmt.Errorf("unable to create request: %w", err)
+	}
+	defer req.Body.Close()
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("X-Postmark-Server-Token", s.emailAPIToken)
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("unable to send request to email provider: %w", err)
+	}
+
+	// Print response status
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println("Response Status: ", resp.Status)
+	fmt.Println("Body: ", string(body))
+
+	return nil
+}
+
+func (s *emailService) createMfaCodeBodyRequest(mfaCode string, fromAddress, toAddress string) (io.Reader, error) {
+	data := &mfaCodeBodyRequest{
+		From:          fromAddress,
+		To:            toAddress,
+		TemplateAlias: "mfa_code",
+		TemplateModel: &templateModel{
+			MfaCode: mfaCode,
+		},
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse json body: %w", err)
+	}
+	return bytes.NewBuffer(jsonData), nil
+}
---
+++ backend/app/infra/email_service_test.go
@@ -0,0 +1,76 @@
+package infra_test
+
+import (
+	"io"
+	"net/http"
+	"testing"
+	"bytes"
+
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/joaopedropio/musiquera/app/infra"
+)
+
+func TestEmailService_EmailAddressValidation(t *testing.T) {
+	service := infra.NewEmailService(nil, "", "")
+	assert.True(t, service.IsValidEmailAddress("musiquera@musiquera.uk"))
+	assert.True(t, service.IsValidEmailAddress("example@gmail.com"))
+	assert.False(t, service.IsValidEmailAddress("email"))
+	assert.False(t, service.IsValidEmailAddress("email@@gmail.com"))
+	assert.False(t, service.IsValidEmailAddress("email@exx gmail"))
+	assert.False(t, service.IsValidEmailAddress("email@.gmail"))
+	assert.False(t, service.IsValidEmailAddress("@gmail"))
+	assert.False(t, service.IsValidEmailAddress("email @gmai"))
+}
+
+func TestEmailService_ShouldContainValidBody(t *testing.T) {
+	// Arrange
+	providerURL := "https://api.postmarkapp.com/email/withTemplate"
+	mfaCode := "123456"
+	toEmailAddress := "to@email.com"
+	fromEmailAddress := "from@email.com"
+	apiToken := "api_token"
+	clientMock := NewHTTPClientMock(func (req *http.Request) (*http.Response, error)  {
+		assert.Equal(t, providerURL, req.URL.String())
+		assert.Equal(t, req.Method, "POST")
+
+		assert.Equal(t, "application/json", req.Header.Get("Accept"))
+		assert.Equal(t, "application/json", req.Header.Get("Content-Type"))
+		assert.Equal(t, apiToken, req.Header.Get("X-Postmark-Server-Token"))
+
+
+		body, err := io.ReadAll(req.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, `{"From":"from@email.com","To":"to@email.com","TemplateAlias":"mfa_code","TemplateModel":{"mfa_code":"123456"}}`, string(body))
+
+		return &http.Response{
+                    StatusCode: 200,
+                    Body:       io.NopCloser(bytes.NewBufferString(`{"success":true}`)),
+                    Header:     make(http.Header),
+                }, nil
+	})
+	emailService := infra.NewEmailService(clientMock, apiToken, fromEmailAddress)
+
+	// Act
+	err	 := emailService.SendCreateAccountMFACode(mfaCode, toEmailAddress)
+
+	// Assert
+	assert.Nil(t, err)
+}
+
+func NewHTTPClientMock(handler func(req *http.Request) (*http.Response, error)) *http.Client {
+	return &http.Client{
+		Transport: &MockRoundTripper{
+			RoundTripFunc: handler,
+		},
+	}
+}
+
+type MockRoundTripper struct {
+	RoundTripFunc func(*http.Request) (*http.Response, error)
+}
+
+func (m *MockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	return m.RoundTripFunc(req)
+}
